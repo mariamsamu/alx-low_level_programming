bb7ef290fe2dd519f7f598dd4bf5b73fee11d8f4
mariamsamu <138806238+mariamsamu@users.noreply.github.com>
2023-08-17 19:59:32 +0300

    C - more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @a: first integer
+* @b: second integer
+* Return: addition of a and b
+* Owned by Maryam Ahmed(mariamsamu)
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#this is my task as C - More functions, more nested loops 
